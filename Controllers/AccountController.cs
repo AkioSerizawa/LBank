7c25c0498b98751d816827026fa7733a720e25e2
Akio Serizawa <akio50@hotmail.com>
2022-11-19 16:03:41 -0300

    Visualização da Conta

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,7 +1,67 @@
+using LBank.Data;
+using LBank.Models;
+using LBank.Utils;
+using LBank.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LBank.Controllers;
 
+[ApiController]
 public class AccountController : ControllerBase
 {
+    [HttpGet("v1/Account/")]
+    public async Task<IActionResult> GetAccountAsync(
+        [FromServices] DataContext context)
+    {
+        try
+        {
+            var account = await context
+                .Accounts
+                .ToListAsync();
+
+            if (account.Count == 0)
+                return NotFound(new ResultViewModel<Account>(UtilMessages.account03XE03()));
+
+            return Ok(new ResultViewModel<List<Account>>(account));
+        }
+        catch (TimeoutException ex)
+        {
+            return StatusCode(408, new ResultViewModel<Account>(UtilMessages.information01XE01(ex)));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<Account>(UtilMessages.account03XE01(ex)));
+        }
+    }
+
+    [HttpGet("v1/account/{id:int}")]
+    public async Task<IActionResult> GetAccountByIdAsync(
+        [FromRoute] int id,
+        [FromServices] DataContext context)
+    {
+        try
+        {
+            var account = await context
+                .Accounts
+                .Include(x => x.User)
+                .FirstOrDefaultAsync(x => x.AccountId == id);
+
+            if (account == null)
+                return NotFound(new ResultViewModel<Account>(UtilMessages.account03XE02(id)));
+
+            var accountView =
+                $"| Num.Conta: {account.AccountId} | Saldo da Conta: {account.AccountBalance} | Usuario: {account.User.UserName} |";
+
+            return Ok(new ResultViewModel<dynamic>(accountView, null));
+        }
+        catch (TimeoutException ex)
+        {
+            return StatusCode(408, new ResultViewModel<Account>(UtilMessages.information01XE01(ex)));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<Account>(UtilMessages.account03XE01(ex)));
+        }
+    }
 }
--- Utils/UtilMessages.cs
+++ Utils/UtilMessages.cs
@@ -2,6 +2,13 @@ namespace LBank.Utils;
 
 public class UtilMessages
 {
+    #region Information
+
+    public static string information01XE01(Exception ex) =>
+        $"01XE01 - TimeOut, Processamento tomou mais tempo do que o permitido | {ex.Message} |";
+
+    #endregion
+
     #region User
 
     public static string user02XE01(Exception ex) => $"02XE01 - Falha interna no servidor - | {ex.Message} |";
@@ -11,6 +18,17 @@ public class UtilMessages
 
     public static string user02XE03() => $"02XE03 - Usuário ou senha inválida";
     public static string user02XE04() => $"02XE04 - Senha inválida";
+
+    #endregion
+
+    #region Account
+
+    public static string account03XE01(Exception ex) => $"03XE01 - Falha interna no servidor - | {ex.Message} |";
+
+    public static string account03XE02(int id) =>
+        $"03XE02 - Nenhuma conta bancaria encontrada! | Conta pesquisada - '{id}' |";
+
+    public static string account03XE03() => $"03XE03 - Nenhuma conta bancaria encontrada!";
 
     #endregion
 }
